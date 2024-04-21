c064bee225fb6cd840dbc4c943b8a9725895000b
Koushik <koushik_2031@yahoo.com>
2024-04-21 22:15:14 +0530

    main code

---
+++ test_1.py
@@ -0,0 +1,45 @@
+from transformers.utils import send_example_telemetry
+from transformers import AutoFeatureExtractor, AutoModel
+import torchvision.transforms as T
+
+import torch 
+
+# send_example_telemetry("image_similarity_notebook", framework="pytorch")
+image_location = "/mnt/d/samples/"
+model_ckpt = "nateraw/vit-base-beans"
+extractor = AutoFeatureExtractor.from_pretrained(model_ckpt)
+model = AutoModel.from_pretrained(model_ckpt)
+hidden_dim = model.config.hidden_size
+
+# Data transformation chain.
+transformation_chain = T.Compose(
+    [
+        # We first resize the input image to 256x256 and then we take center crop.
+        T.Resize(int((256 / 224) * extractor.size["height"])),
+        T.CenterCrop(extractor.size["height"]),
+        T.ToTensor(),
+        T.Normalize(mean=extractor.image_mean, std=extractor.image_std),
+    ]
+)
+
+def extract_embeddings(model: torch.nn.Module):
+    """Utility to compute embeddings."""
+    device = model.device
+
+    def pp(batch):
+        images = batch["image"]
+        # `transformation_chain` is a compostion of preprocessing
+        # transformations we apply to the input images to prepare them
+        # for the model. For more details, check out the accompanying Colab Notebook.
+        image_batch_transformed = torch.stack(
+            [transformation_chain(image) for image in images]
+        )
+        new_batch = {"pixel_values": image_batch_transformed.to(device)}
+        with torch.no_grad():
+            embeddings = model(**new_batch).last_hidden_state[:, 0].cpu()
+        return {"embeddings": embeddings}
+
+    return pp
+
+
+
